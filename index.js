5025de4bdad8e40d8808dd8e7c4b751196f4247f
deepak-padampur <deepaksuna4290@gmail.com>
2021-03-18 08:59:17 +0530

    port number changed

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.use(errorHandler)
 
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is up and running at port ${PORT}`)
 })
